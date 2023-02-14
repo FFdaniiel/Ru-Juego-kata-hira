73783a98de91de86736687d6ff1da981599f0282
unknown <daniel_alexis_12@hotmail.com>
2023-02-14 19:10:47 -0300

    agregando js, para poder ejecutar el juego en hiragana

--- js/app.js
+++ js/app.js
@@ -1,2 +1,45 @@
 const silabaRandom = document.querySelector('#silabaRandom');
+const btnEnviarDato = document.querySelector('#btnEnviar');
+const respuestaTexto = document.querySelector('#respuesta');
 
+let contadorIntentos = 0;
+let contadorAciertos = 0;
+let silabaActual = []
+let respuestaCorrecta = []
+
+// Se crea  una constante para generar una aleatoridad en el array de silabas
+function InicioLetrahi (){
+    const posicionAleatoria = Math.floor(Math.random()*hiragana.length);
+    respuestaCorrecta = hiraRoma[posicionAleatoria];
+    silabaActual = hiragana[posicionAleatoria];
+    silabaRandom.innerHTML = silabaActual;
+    verificarRespuesta()
+}
+
+// Verifica si el texto colocado es igual que el simbolo 
+
+function verificarRespuesta(){
+    respuestaTexto.addEventListener ("click", () => {
+        let textAreaDeRespuesta = document.querySelector('#respuesta').value;
+        if (respuestaCorrecta == textAreaDeRespuesta) {
+            respuestaTexto.value = '';
+            contadorAciertos++
+            InicioLetrahi ()
+        }else if(respuestaCorrecta != textAreaDeRespuesta){
+            console.log(`apa, es ${respuestaCorrecta}`);
+            contadorIntentos++
+        }
+    
+    });
+
+}
+// Cancelar el salto entre linea de texto / es decir cancela enter
+function cancelar() {
+    if (e.keyCode === 13 && !e.shiftKey) {
+        e.preventDefault();
+        var boton = document.getElementById("boton");
+        angular.element(boton).triggerHandler('click');
+    }
+}
+
+InicioLetrahi();
